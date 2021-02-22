265ac9193cc0907662f972fc6021299848fa2159
John <john@programbo.com>
2021-02-22 12:17:46 +0800

    Stringify params for PostCSS atRules

--- index.js
+++ index.js
@@ -2,16 +2,16 @@ const plugin = require('tailwindcss/plugin');
 
 module.exports = plugin(function ({ addVariant, e, postcss }) {
   addVariant('retina', ({ container, separator }) => {
-    const supportsRule = postcss.atRule({
+    const mediaQuery = postcss.atRule({
       name: 'media',
       params: [
         '(-webkit-min-device-pixel-ratio: 2)',
         '(min-resolution: 192dpi)',
-      ],
+      ].join(),
     });
-    supportsRule.append(container.nodes);
-    container.append(supportsRule);
-    supportsRule.walkRules(rule => {
+    mediaQuery.append(container.nodes);
+    container.append(mediaQuery);
+    mediaQuery.walkRules(rule => {
       rule.selector = `.${e(`retina${separator}${rule.selector.slice(1)}`)}`;
     });
   });
